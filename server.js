224ef7fdcf16acdf51f05bad8311a615781cf66f
Trent Roesbery <xtrent777x@gmail.com>
2021-10-08 16:44:16 -0600

    main server js file setup and npm i completed

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-// file structure setup from model for commit
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+// pizza hunt set up
