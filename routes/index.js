a68fbd260b3a958edf920f452839a93d3185fc0d
Trent Roesbery <xtrent777x@gmail.com>
2021-10-08 18:57:19 -0600

    api route index file

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
-// stucture from module to commit 
+
+
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+module.exports = router;
