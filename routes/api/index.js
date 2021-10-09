b5411dcd863e9b172a0062a53042a0a5013bac2a
Trent Roesbery <xtrent777x@gmail.com>
2021-10-08 18:52:52 -0600

    api index and user

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,8 @@
-// stucture from module to commit 
+const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
+const usersRoutes = require('./user-routes');
+
+router.use('/thought', thoughtRoutes);
+router.use('/users', usersRoutes);
+
+module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,1 +1,23 @@
-// stucture from module to commit 
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getUsersById,
+  createUsers,
+  updateUsers,
+  deleteUsers
+} = require('../../controllers/users-controller');
+
+// /api/users
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUsers);
+
+// /api/Users/:id
+router
+  .route('/:id')
+  .get(getUsersById)
+  .put(updateUsers)
+  .delete(deleteUsers);
+
+module.exports = router;
