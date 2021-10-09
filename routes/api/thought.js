1daccd0992c5eab7cb59fd39d188624ecc0a60d7
Trent Roesbery <xtrent777x@gmail.com>
2021-10-08 20:58:06 -0600

    thought api

--- routes/api/thought.js
+++ routes/api/thought.js
@@ -1,1 +1,17 @@
-// stucture from module to commit 
+const router = require('express').Router();
+const {
+  addThought,
+  removeThought
+} = require('../../controllers/thought-controller');
+
+// /api/thought/<usersId>
+router.route('/:usersId').post(addThought);
+
+// /api/comments/<usersId>/<thoughtId>
+router
+  .route('/:usersId/:thoughtId')
+  .delete(removeThought);
+
+
+
+module.exports = router;
